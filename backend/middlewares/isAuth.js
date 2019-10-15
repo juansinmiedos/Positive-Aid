c68b4ee99642179228e10f04e4d93b807adb58c2
Juan Vidal <juanvidaldom@gmail.com>
2019-10-15 09:14:09 -0500

    test to erase node modules, oh please work

--- backend/middlewares/isAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.isAuthenticated() && req.user.confirmationStatus === "Active") {
-      next();
-    } else {
-      res.status(401).json({ msg: 'Log in first' });
-    }
-};
